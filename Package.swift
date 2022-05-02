19bd8d4adc9c1d3b2b7d8ce4a53e3f41616b2fa1
kofno <ryan.l.bell@gmail.com>
2022-05-02 10:10:08 -0400

    Remove SPM deprecated API usage

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     .library(name: "SwiftDecodePipeline", targets: ["SwiftDecodePipeline"]),
   ],
   dependencies: [
-    .package(name: "SwiftyJSON", url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
-    .package(name: "Curry", url: "https://github.com/thoughtbot/Curry.git", from: "5.0.0"),
+    .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
+    .package(url: "https://github.com/thoughtbot/Curry.git", from: "5.0.0"),
   ],
   targets: [
     .target(name: "SwiftDecodePipeline", dependencies: [.byName(name: "SwiftyJSON"), .byName(name: "Curry")]),
